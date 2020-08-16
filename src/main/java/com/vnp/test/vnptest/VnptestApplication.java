4c15580f92e6643080f95a75f14ca078c9a15b26
veera <veera@veeras-mbp.fios-router.home>
2020-08-16 16:35:03 -0400

    First commit

---
+++ src/main/java/com/vnp/test/vnptest/VnptestApplication.java
@@ -0,0 +1,14 @@
+package com.vnp.test.vnptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnptestApplication.class, args);
+		System.out.println("Checkin-1");
+	}
+
+}
